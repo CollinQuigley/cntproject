67c3100340fe57b1b4a02197cd436f9d258cbee6
Aiden Gaul <100671205+aidengaul@users.noreply.github.com>
2024-03-18 02:51:19 -0400

    Add initial implementation of getHaveMessage

--- src/Messages.java
+++ src/Messages.java
@@ -1,4 +1,7 @@
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.BitSet;
 
 public class Messages {
 
@@ -15,6 +18,27 @@ public class Messages {
         buffer.putInt(peerID);
 
         return buffer.array();
+    }
+
+    public static byte[] getHaveMessage(BitSet bf) throws IOException {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        // Start with 1 byte for the message type field
+        int length = 1;
+
+        byte[] payload = bf.toByteArray();
+        length += payload.length;
+
+        // create a 4 byte buffer to hold the length variable
+        ByteBuffer buffer = ByteBuffer.allocate(4);
+        buffer.putInt(length);
+        buffer.flip();
+
+        stream.write(buffer.array());
+        stream.write(5);
+        stream.write(payload);
+        stream.close();
+
+        return stream.toByteArray();
     }
 
 }
