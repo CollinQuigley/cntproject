bf5046034df293e190749ebfe7f029dc46e79edd
Aiden Gaul <100671205+aidengaul@users.noreply.github.com>
2024-03-18 04:17:25 -0400

    Restructure ConnectionThread.java, add readMessage() to process non-handshake messages

--- src/ConnectionThread.java
+++ src/ConnectionThread.java
@@ -40,51 +40,34 @@ public class ConnectionThread extends Thread{
 
         System.out.println(peer.getPeerID() + " has started TCP connection with peer process " + remoteHost + " " + remotePort);
 
-        if(!handshake){
-            if(isInitiatedByThisProcess){
-                byte[] handshakeMessage = Messages.getHandShakeMessage(peer.getPeerID());
-                sendMessage(handshakeMessage, out);
-                byte[] handShakeMessage = readHandShakeMessage(in);
-                String handshakeHeader = new String(handShakeMessage, 0, 18);
-                System.out.println("Handshake Header: " + handshakeHeader);
-
-                // Extract zero bits (next 10 bytes)
-                String zeroBits = new String(handShakeMessage, 18, 10);
-                System.out.println("Zero Bits: " + zeroBits);
-
-                // Extract peer ID (last 4 bytes)
-                ByteBuffer buffer = ByteBuffer.wrap(handShakeMessage, 28, 4);
-                int remotePeerID = buffer.getInt();
-                System.out.println("Peer ID: " + remotePeerID);
-                peer.fileLogger.makeConnection(remotePeerID);
-
-
-            }
-            else{
-                byte[] handShakeMessage = readHandShakeMessage(in);
-                String handshakeHeader = new String(handShakeMessage, 0, 18);
-                System.out.println("Handshake Header: " + handshakeHeader);
-
-                // Extract zero bits (next 10 bytes)
-                String zeroBits = new String(handShakeMessage, 18, 10);
-                System.out.println("Zero Bits: " + zeroBits);
-
-                // Extract peer ID (last 4 bytes)
-                ByteBuffer buffer = ByteBuffer.wrap(handShakeMessage, 28, 4);
-                int remotePeerID = buffer.getInt();
-                System.out.println("Peer ID: " + remotePeerID);
-                byte[] handshakeMessage = Messages.getHandShakeMessage(peer.getPeerID());
-                sendMessage(handshakeMessage, out);
-                peer.fileLogger.isConnected(remotePeerID);
-
-
-            }
-
-
+        // initial handshake
+        try {
+            if(!handshake)
+                doHandshake();
+        }
+        catch (IOException e) {
+            System.out.println(e);
         }
 
+        try{
+            byte[] newMessage = in.readAllBytes();
+            int messageType = newMessage[4];
 
+            switch(messageType) {
+                // have message
+                case 5:
 
+                    break;
+
+                // default catch all
+                default:
+                    System.out.println("Unsupported message type " + messageType);
+                    break;
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
     private void sendMessage(byte[] message, DataOutputStream out)  {
@@ -97,11 +80,48 @@ public class ConnectionThread extends Thread{
         }
     }
 
+    public void doHandshake() throws IOException {
+        if(isInitiatedByThisProcess){
+            byte[] handshakeMessage = Messages.getHandShakeMessage(peer.getPeerID());
+            sendMessage(handshakeMessage, out);
+            byte[] handShakeMessage = readHandShakeMessage(in);
+            String handshakeHeader = new String(handShakeMessage, 0, 18);
+            System.out.println("Handshake Header: " + handshakeHeader);
+
+            // Extract zero bits (next 10 bytes)
+            String zeroBits = new String(handShakeMessage, 18, 10);
+            System.out.println("Zero Bits: " + zeroBits);
+
+            // Extract peer ID (last 4 bytes)
+            ByteBuffer buffer = ByteBuffer.wrap(handShakeMessage, 28, 4);
+            int remotePeerID = buffer.getInt();
+            System.out.println("Peer ID: " + remotePeerID);
+            peer.fileLogger.makeConnection(remotePeerID);
+        }
+        else{
+            byte[] handShakeMessage = readHandShakeMessage(in);
+            String handshakeHeader = new String(handShakeMessage, 0, 18);
+            System.out.println("Handshake Header: " + handshakeHeader);
+
+            // Extract zero bits (next 10 bytes)
+            String zeroBits = new String(handShakeMessage, 18, 10);
+            System.out.println("Zero Bits: " + zeroBits);
+
+            // Extract peer ID (last 4 bytes)
+            ByteBuffer buffer = ByteBuffer.wrap(handShakeMessage, 28, 4);
+            int remotePeerID = buffer.getInt();
+            System.out.println("Peer ID: " + remotePeerID);
+            byte[] handshakeMessage = Messages.getHandShakeMessage(peer.getPeerID());
+            sendMessage(handshakeMessage, out);
+            peer.fileLogger.isConnected(remotePeerID);
+        }
+    }
+
     private byte[] readHandShakeMessage(DataInputStream in){
         byte[] handshakeMessage = new byte[32];
 
         try {
-           in.readFully(handshakeMessage);
+            in.readFully(handshakeMessage);
         }
         catch (IOException e){
             throw new RuntimeException(e);
@@ -109,7 +129,28 @@ public class ConnectionThread extends Thread{
         return handshakeMessage;
     }
 
+    private byte[] readMessage(DataInputStream in) {
+        // allocate 4 bytes then wrap to bytebuffer to get integer value of message length
+        byte[] lengthBytes = new byte[4];
+        byte[] message;
+        int messageLength = 0;
+        ByteBuffer bf;
 
+        try {
+            in.readFully(lengthBytes);
+            bf = ByteBuffer.wrap(lengthBytes);
+            messageLength = bf.getInt();
+
+            // create new byte array and readFully into it, then append remaining message to bf
+            message = new byte[messageLength + 4];
+            System.arraycopy(lengthBytes, 0, message, 0, 4);
+            in.readFully(message, 4, messageLength - 4);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return message;
+    }
 }
 
 
